14e991faccd09b62d3087ba83de7af1002849f8d
SivakumarRevuri <sivakumarr96@gmail.com>
2019-07-17 21:29:35 +0530

    Collection frequency()

--- src/com/shiv/collection/ArrayListDemo.java
+++ src/com/shiv/collection/ArrayListDemo.java
@@ -1,24 +1,25 @@
-package com.shiv.list;
+package com.shiv.collection;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArrayListDemo {
+	
 	public static void main(String[] args) {
-		ArrayList<Integer> arrayList = new ArrayList<>();
+		List<Integer> arrayList = new ArrayList<>();
 		arrayList.add(43);
 		arrayList.add(12);
 		arrayList.add(34);
 		arrayList.add(43);
-		arrayList.add(75);
 		// array list allows duplicates and maintains insertion order
-		System.out.println(arrayList); // [43, 12, 34, 43, 75]
+		System.out.println(arrayList); // [43, 12, 34, 43]
 
 		arrayList.add(0, 99);
 		arrayList.set(2, 65);
-		System.out.println(arrayList); // [99, 43, 65, 34, 43, 75]
+		System.out.println(arrayList); // [99, 43, 65, 34, 43]
 
 		System.out.println(arrayList.contains(100)); // false
 		arrayList.remove(1);
-		System.out.println(arrayList); // [99, 65, 34, 43, 75]
+		System.out.println(arrayList); // [99, 65, 34, 43]
 	}
 }
